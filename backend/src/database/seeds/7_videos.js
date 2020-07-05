a54568ca2e73659985a5a8eaf187fb46ad2644d5
Lucas Emanuel <lucasnobrega.js@gmail.com>
2020-07-05 20:12:54 -0300

    Corrigindo os links do youtube

--- backend/src/database/seeds/7_videos.js
+++ backend/src/database/seeds/7_videos.js
@@ -5,14 +5,14 @@ exports.seed = function(knex) {
       id: 1, 
       userId: 1,
       bookId: 1,
-      videoName: 'https://www.youtube.com/watch?v=Z3mG0aVjgGM',
+      videoName: 'https://www.youtube.com/embed/Z3mG0aVjgGM',
       likes: 50
     },
     {
       id: 2, 
       userId: 2,
       bookId: 2,
-      videoName: 'https://www.youtube.com/watch?v=BexHqTwglHM',
+      videoName: "https://www.youtube.com/embed/BexHqTwglHM",
       likes: 365
     },
   ]);
