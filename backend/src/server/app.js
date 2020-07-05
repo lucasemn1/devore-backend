3ae0740f1961d4623782ce9b0792f1d4dc57043d
Lucas Emanuel <lucasnobrega.js@gmail.com>
2020-07-05 20:42:49 -0300

    Inserindo listagem de mensagens

--- backend/src/server/app.js
+++ backend/src/server/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
+const cors = require('cors');
 
 app.use('/public', express.static(path.resolve(__dirname, '..', '..', 'public')))
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
