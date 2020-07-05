7ad78a312101d1e6cd3f0e0ce59e77f7823b7196
Lucas Emanuel <lucasnobrega.js@gmail.com>
2020-07-05 12:01:41 -0300

    Correção do retorno de erro no middleware de autenticação

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -4,7 +4,7 @@ module.exports = async function(request, response, next) {
   const accessId = String(request.header('access_id'));
 
   if( accessId === 'undefined' ) {
-    return response.status(401).send('O id de acesso é requerido');
+    return response.status(401).json({ message: 'O id de acesso é requerido'});
   }
 
   const accessIdIsValid = await connection('users')
@@ -12,7 +12,7 @@ module.exports = async function(request, response, next) {
     .where('users.accessId', '=', accessId);
 
   if( accessIdIsValid.length === 0 ) {
-    return response.status(401).send('O id não está registrado');
+    return response.status(401).json({ message: 'O id não está registrado'});
   }
 
   next();
